f0a0de81a4e810e16b4926688dbf9b87688d4e9c
Justin Patrick <jpatrick6420@gmail.com>
2023-04-24 07:21:17 -0600

    added glossary page

--- learning-storis.js
+++ learning-storis.js
@@ -5,9 +5,19 @@ const training = document.getElementById('tutorial');
 const hideVideo = document.getElementById('antiVideo');
 const instruction = document.getElementById('manual');
 
+
+// search button
+
+
+
 //sales order var
 
-const salesimages= ['./images/salesorder.jpeg', './images/customerScreen.jpeg', './images/fullfilment.jpeg', './images/payment.jpeg' ]
+
+
+
+
+
+const salesimages = ['./images/salesorder.jpeg', './images/customerScreen.jpeg', './images/fullfilment.jpeg', './images/payment.jpeg']
 
 
 const salesOrderNotes = document.getElementById('salesOrderNotes');
@@ -36,93 +46,97 @@ const CloseButton = document.getElementById('closeInstructions');
 
 
 
-function enlargeMain(){
-    main.style.width === "10vw" ? main.style.width = '50vw' : main.style.width = '10vw';};
-function enlargeInquiry(){
-    inquiry.style.width === "10vw" ? inquiry.style.width = '50vw' : inquiry.style.width = '10vw';};
+function enlargeMain() {
+    main.style.width === "10vw" ? main.style.width = '50vw' : main.style.width = '10vw';
+};
+function enlargeInquiry() {
+    inquiry.style.width === "10vw" ? inquiry.style.width = '50vw' : inquiry.style.width = '10vw';
+};
 
 
 //Sales functions
 
 
-function Tutorial(){
-    
-    salesOrderNotes.style.display === 'none' ? salesOrderNotes.style.display = 'block' : salesOrderNotes.style.display = "none";}
+function Tutorial() {
+
+    salesOrderNotes.style.display === 'none' ? salesOrderNotes.style.display = 'block' : salesOrderNotes.style.display = "none";
+}
 
 
 
-function cierreVideo(){
+function cierreVideo() {
     training.style.display === 'none';
-    hideVideo.style.display= 'none';
-    salesOrderNotes.style.display='none';
+    hideVideo.style.display = 'none';
+    salesOrderNotes.style.display = 'none';
 }
 
-function displaySearchTips(){
+function displaySearchTips() {
     search.style.display === 'none' ? search.style.display = 'block' :
-    search.style.display = 'none';
+        search.style.display = 'none';
 
-    
-}
-function openModifyInstructions(){
- if (descriptionField.style.display === 'none') {descriptionField.style.display = 'block'; }
- else {descriptionField.style.display = 'none'}
-}
 
-function openFulfilmentInstructions(){
-    fulfilmentInstructions.style.display === 'none' ? 
-    fulfilmentInstructions.style.display='block': fulfilmentInstructions.style.display = 'none';
+}
+function openModifyInstructions() {
+    if (descriptionField.style.display === 'none') { descriptionField.style.display = 'block'; }
+    else { descriptionField.style.display = 'none' }
 }
 
-
-
-function multipleSalesPerson(){
-salesPerson.style.display === "none" ? salesPerson.style.display= "block" : salesPerson.style.display = "none"
+function openFulfilmentInstructions() {
+    fulfilmentInstructions.style.display === 'none' ?
+        fulfilmentInstructions.style.display = 'block' : fulfilmentInstructions.style.display = 'none';
 }
 
-function openSalesOrder(){
-salesOrderExample.style.width === "25px" ? salesOrderExample.style.width = "800px" : salesOrderExample.style.width = "25px";}
-
-function openMerchandiseScreen(){document.getElementById('merchandiseImage').style.width  === "25px" ? document.getElementById('merchandiseImage').style.width = "800px" : document.getElementById('merchandiseImage').style.width = "25px"};
 
 
-function openSearchScreen(){document.getElementById('searchImage').style.width  === "25px" ? document.getElementById('SearchImage').style.width = "800px" : document.getElementById('searchImage').style.width = "25px"};
+function multipleSalesPerson() {
+    salesPerson.style.display === "none" ? salesPerson.style.display = "block" : salesPerson.style.display = "none"
+}
 
-function openFullfilmentScreen(){document.getElementById('fullfilmentImage').style.width  === "25px" ? document.getElementById('fullfilmentImage').style.width = "800px" : document.getElementById('fullfilmentImage').style.width = "25px"};
+function openSalesOrder() {
+    salesOrderExample.style.width === "25px" ? salesOrderExample.style.width = "800px" : salesOrderExample.style.width = "25px";
+}
 
-function openPaymentImage(){document.getElementById('paymentImage').style.width  === "25px" ? document.getElementById('paymentImage').style.width = "800px" : document.getElementById('paymentImage').style.width = "25px"};
+function openMerchandiseScreen() { document.getElementById('merchandiseImage').style.width === "25px" ? document.getElementById('merchandiseImage').style.width = "800px" : document.getElementById('merchandiseImage').style.width = "25px" };
+
+
+function openSearchScreen() { document.getElementById('searchImage').style.width === "25px" ? document.getElementById('SearchImage').style.width = "800px" : document.getElementById('searchImage').style.width = "25px" };
+
+function openFullfilmentScreen() { document.getElementById('fullfilmentImage').style.width === "25px" ? document.getElementById('fullfilmentImage').style.width = "800px" : document.getElementById('fullfilmentImage').style.width = "25px" };
+
+function openPaymentImage() { document.getElementById('paymentImage').style.width === "25px" ? document.getElementById('paymentImage').style.width = "800px" : document.getElementById('paymentImage').style.width = "25px" };
 
 //Productivity Information
 
 
- function PlayProductActivity(){
-    
-    productivity.style.display === "none" ? productivity.style.display = "block" : productivity.style.display = "none" ;
- }
+function PlayProductActivity() {
+
+    productivity.style.display === "none" ? productivity.style.display = "block" : productivity.style.display = "none";
+}
 
 
 
 //sales activity functions
 
 
- 
- function PlaySalesActivity(){
-    salesActivity.style.display === "none" ? salesActivity.style.display = "block" : salesActivity.style.display = "none" ;
- }
 
- function DeliverySchedule(){
- 
-    delivery.style.display === 'none' ? delivery.style.display='block' : delivery.style.display='none';
- }
+function PlaySalesActivity() {
+    salesActivity.style.display === "none" ? salesActivity.style.display = "block" : salesActivity.style.display = "none";
+}
 
- 
+function DeliverySchedule() {
+
+    delivery.style.display === 'none' ? delivery.style.display = 'block' : delivery.style.display = 'none';
+}
+
+
 //closes all tutorial windows
 
 
-function closeInstructions(){
-    salesOrderNotes.style.display="none";
+function closeInstructions() {
+    salesOrderNotes.style.display = "none";
     modifyInstructions.style.display = "none";
-    salesActivity.style.display="none";
-    productivity.style.display="none";
-    delivery.style.display="none";
+    salesActivity.style.display = "none";
+    productivity.style.display = "none";
+    delivery.style.display = "none";
 
 }
