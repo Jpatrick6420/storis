6dcc43293b2a5d4d8e488f2e53547ab2c79612b0
Justin Patrick <jpatrick6420@gmail.com>
2023-08-24 17:23:16 -0600

    cleaned up the css/ javascript files removed, excess code

--- learning-storis.js
+++ learning-storis.js
@@ -5,7 +5,6 @@ const salesimages = [
   "./images/payment.jpeg",
 ];
 
-// const salesOrderNotes = document.getElementById("salesOrderNotes");
 const searchTips = document.getElementById("searchTips");
 const modifyInstructions = document.getElementById("modifyInstructions");
 const multipleFullfilments = document.getElementById("multipleFulfillments");
@@ -14,7 +13,6 @@ const descriptionField = document.getElementById("DescriptionField");
 const fulfilmentInstructions = document.getElementById(
   "FulfilmentInstructions"
 );
-// const merchandiseImage = document.querySelector("#merchandiseImage");
 
 const multSalesPeople = document.getElementById("multSalesPeople");
 
@@ -35,14 +33,6 @@ function openFulfilmentInstructions() {
 const enlargePicture = (picture) =>
   (picture.style.width = picture.style.width === "25px" ? "400px" : "25px");
 
-// function openMerchandiseScreen() {
-//   enlargePicture(merchandiseImage);
-// }
-
-function openSearchScreen() {
-  enlargePicture(document.getElementById("searchImage"));
-}
-
 function openFullfilmentScreen() {
   enlargePicture(document.getElementById("fullfilmentImage"));
 }
@@ -59,32 +49,15 @@ function PlayProductActivity() {
 
 //sales activity functions
 
-function PlaySalesActivity() {
-  openMenu(salesActivity);
-}
+// function PlaySalesActivity() {
+//   openMenu(salesActivity);
+// }
 
 function DeliverySchedule() {
   openMenu(delivery);
 }
 
-//closes all tutorial windows
-
-// function closeInstructions() {
-//   salesOrderNotes.style.display = "none";
-//   modifyInstructions.style.display = "none";
-//   salesActivity.style.display = "none";
-//   productivity.style.display = "none";
-//   delivery.style.display = "none";
-// }
-
-// manage open orders section
-
 const manageOpenOrders = document.querySelector(".manage-open-orders");
-// const showManageOpenOrders= document.querySelector(".show-manage-open-orders");
-// showManageOpenOrders.addEventListener("click", () => {
-
-//     manageOpenOrders.style.display === "none" ? manageOpenOrders.style.display = "block" : manageOpenOrders.style.display = "none";
-// })
 
 const showAddNotes = document.querySelector(".show-add-notes");
 const addNotes = document.querySelector(".add-notes");
--- pages/manage_undelivered/manage_undelivered.js
+++ pages/manage_undelivered/manage_undelivered.js
@@ -1,4 +1,16 @@
 const showNotes = document.querySelector(".show-add-notes");
-showNotes.addEventListener("click", function () {
-  removeHiddenClass(document.querySelector(".add-notes"));
+const setPickupOptions = document.querySelector(".set-pickup-options");
+
+// showNotes.addEventListener("click", function () {
+//   removeHiddenClass(document.querySelector(".add-notes"));
+// });
+
+const viewOptions = document.querySelector("#view-options");
+
+viewOptions.addEventListener("click", function () {
+  if (setPickupOptions.classList.contains("hidden")) {
+    removeHiddenClass(setPickupOptions);
+  } else {
+    setPickupOptions.classList.add("hidden");
+  }
 });
